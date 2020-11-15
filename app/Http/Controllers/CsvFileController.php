b5142116f1188722af7cf325e3af5a7d73d703db
Adrián <adriangsanchez98@gmail.com>
2020-11-15 20:21:15 +0000

    Add migration for Csv files, and add relation on models.

---
+++ app/Http/Controllers/CsvFileController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CsvFile;
+use Illuminate\Http\Request;
+
+class CsvFileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CsvFile  $csvFile
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CsvFile $csvFile)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\CsvFile  $csvFile
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CsvFile $csvFile)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CsvFile  $csvFile
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CsvFile $csvFile)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CsvFile  $csvFile
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CsvFile $csvFile)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_11_15_200126_create_csv_files_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCsvFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('csv_files', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            // Simplicity, else I would use the File System.
+            $table->longText('csv_file');
+            $table->string('name', 191);
+            $table->foreign('user_id')->references('id')->on('users')->restrictOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('csv_files');
+    }
+}
